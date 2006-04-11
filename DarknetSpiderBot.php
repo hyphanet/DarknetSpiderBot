d52eb630cbf5aa5a02e67184503785db2b446074
dongy <dongy@unknown>
2006-04-11 19:07:06 +0000

    getLastEdition added restructuring project
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/apps/DarknetSpiderBot@8520 67a373e5-eb02-0410-a15c-ee090a768436

--- DarknetSpiderBot.php
+++ DarknetSpiderBot.php
@@ -1,74 +1,97 @@
 <?php
-set_time_limit(60);
+set_time_limit(90);
 
-require_once('config.php'); 
+require_once('include/config.inc.php'); 
 
-//$url = $addresse_fcp.$start_page;
-//$url = 'http://www.lemonde.fr/';
-
-$sitekey = 'PFeLTa1si2Ml5sDeUy7eDhPso6TPdmw-2gWfQ4Jg02w,3ocfrqgUMVWA2PeorZx40TW0c-FiIOL-TWKQHoDbVdE,AQABAAE';
-$sitename = 'Index';
-
-$sitepath = "/USK@$sitekey/$sitename";
 
 $buffer_file = 'local.html';
 
-$bot = new bot();
+$site_key = 'PFeLTa1si2Ml5sDeUy7eDhPso6TPdmw-2gWfQ4Jg02w,3ocfrqgUMVWA2PeorZx40TW0c-FiIOL-TWKQHoDbVdE,AQABAAE';
+$site_name = 'Index';
+$last_know_edition = '20';
 
-$bot->getDistantFile($buffer_file, $fcp_host, $fcp_port, $sitepath.'/-1');
-//$bot->buffer_contents = $bot->getFileContents($buffer_file);
+
+
+$bot = new bot($fcp_host, $fcp_port, $buffer_file);
+
+echo $bot->getLastEdition($site_key, $site_name, $last_know_edition);
+
+//$sitepath = "/USK@$sitekey/$sitename";
+//$bot->getDistantFile($fcp_host, $fcp_port, $sitepath.'/-1');
+
 
 //$urls = $bot->extractURLs();
 //$bot->cleanURLs($urls, $sitekey, $sitename);
 
 //print_r($urls);
-//echo $bot->buffer_contents;
+//echo $bot->buffer;
+
+echo "\r\nDarknetSpiderBot is closing...\r\n";
+
 
 
 class bot {
 	
-	var $buffer_contents;
+	var $fcp_host;
+	var $fcp_port;
 	
-	function getDistantFile ($buffer_file, $fcp_host, $fcp_port, $sitepath='')
+	var $buffer;
+	var $buffer_file;
+	
+	
+	function bot ($fcp_host, $fcp_port, $buffer_file)
 	{
-		global $timeout, $wget_dir;
+		$this->fcp_host = $fcp_host;
+		$this->fcp_port = $fcp_port;
+		$this->buffer_file = $buffer_file;
+	}
+	
+	function getDistantFile ($path, $timeout=30)
+	{
 		
-		//exec($wget_dir."wget.exe --timeout=$timeout ${fcp}$sitepath -O $buffer_file");
-		
-		$fp = fsockopen($fcp_host, $fcp_port, $errno, $errstr, $timeout);
+		$fp = fsockopen($this->fcp_host, $this->fcp_port, $errno, $errstr, $timeout);
 		if (!$fp) {
 			echo "$errstr ($errno)<br />\n";
 		}
 		else
 		{
-			$out = "GET $sitepath HTTP/1.1\r\n";
+			$out = "GET $path HTTP/1.1\r\n";
 			$out .= "Host: $fcp_host\r\n";
 			$out .= "Connection: Close\r\n\r\n";
 		
 			fwrite($fp, $out);
 			
-			while (!feof($fp)) {
-				echo fgets($fp, 128);
+			while ( !feof($fp) )
+			{
+				$buffer .= fgets($fp, 4096);
 			}
 			fclose($fp);
 		}
 		
-		$this->buffer_contents = $this->getFileContents($buffer_file);
+		$this->buffer = $buffer;
 	}
 	
-	function getFileContents ($file)
+	function getFileContents ($filename)
 	{
 		
-		$handle = fopen($file, 'r') or die('Erreur à l\'ouverture du fichier'.$file);
-		$contents = fread($handle, filesize ($file));
+		$handle = fopen($filename, 'r') or die("Error during open \"$filename\"");
+		$contents = fread($handle);
 		fclose($handle);
 		
 		return $contents;
 	}
 	
-	function getLastEdition ()
+	function getLastEdition ($site_key, $site_name, $last_know_edition)
 	{
+		$path = "/USK@$site_key/$site_name/-$last_know_edition";
+		//$path = "/USK@$site_key/$site_name/$last_know_edition";
 		
+		$this->getDistantFile($path, 60);
+		
+		if ( preg_match('/\nLocation: (.+)/', $this->buffer, $matches) )
+			return $matches[1];
+			
+		return false;
 	}
 	
 	function extractTitle ()
@@ -148,51 +171,5 @@ class bot {
 	
 
 }
-
-
-/*
-$addresse_complete = "$addresse_fcp" . "$start_page";
-
-exec("c:\wget\wget.exe --timeout=$timeout $addresse_complete -O c:\serveur\www\freenetbot\local.html");
-
-
-
-
-$fich='local.html';
-$ouvre=fopen($fich,'r');
-$filesize = filesize("local.html");
-
-
-while(!feof($ouvre))
-{
-	$ligne=fgets($ouvre,$filesize);
-	
-	if (eregi("<title>(.*)</title>", $ligne, $titre) == TRUE) {
-		//echo $titre[1];
-	}
-	
-	if (eregi("<a(.*)>(.*)</a>", $ligne, $liens) == TRUE) {
-		$liens_complet = $liens[0];
-		$test = explode("href=",$liens_complet);
-		$testa = $test[1];
-		$test1 = explode("\"",$testa);
-		$testb = $test1[1];
-		
-		if (eregi("newbookmark",$testb) == TRUE) { }
-		elseif (eregi("@",$testb) == TRUE) {
-			$cible = "$addresse_fcp" . "$testb";
-			echo "externe : $cible<br>";
-		}
-		else { 
-			$cible = "$addresse_complete" . "$testb";
-			echo "interne : $cible<br>"; 
-		}
-		//exit();
-	}
-    break;
-}
-
-fclose($ouvre);
-*/
 
 ?>
--- include/config.inc.php
+++ include/config.inc.php
@@ -4,11 +4,12 @@ $timeout = "20";
 $fcp_host = "127.0.0.1";
 $fcp_port = '8888';
 
-$wget_dir = 'c:\\wget\\';
-
+/*
 $hostname_bot = "";
 $database_bot = "";
 $username_bot = "";
 $password_bot = "";
-//$bot = mysql_pconnect($hostname_bot, $username_bot, $password_bot) or trigger_error(mysql_error(),E_USER_ERROR); 
+$bot = mysql_pconnect($hostname_bot, $username_bot, $password_bot) or trigger_error(mysql_error(),E_USER_ERROR); 
+*/
+
 ?>
