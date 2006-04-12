8fade32d82b3d3281b4169fab0b3e53287906c8e
sich <sich@freenetproject.org>
2006-04-12 21:12:39 +0000

    DarknetSpiderBot : Add error management (1 files)
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/apps/DarknetSpiderBot@8532 67a373e5-eb02-0410-a15c-ee090a768436

--- fcp.php
+++ fcp.php
@@ -7,6 +7,8 @@ $fcp_port = '9481';
 
 $addresse = "SSK@PFeLTa1si2Ml5sDeUy7eDhPso6TPdmw-2gWfQ4Jg02w,3ocfrqgUMVWA2PeorZx40TW0c-FiIOL-TWKQHoDbVdE,AQABAAE/Index-21/";
 
+$ok_hello = 0;
+$error = NULL;
 
 $fp = fsockopen($fcp_host,$fcp_port,$errno, $errstr, $timeout);
 if (!$fp) {
@@ -25,80 +27,91 @@ else
 	{  
 		$buffer = fgets($fp);
 
-		if (preg_match_all('/NodeHello/si', $buffer, $match)) { 
-			//echo "ok connection\n";
-			//break; 
-		}
-		elseif (preg_match_all('/ProtocolError/si', $buffer, $match)) { 
-			echo "non ok connection\n";
+		if (preg_match_all('/ProtocolError/si', $buffer, $match)) { 
+			$error = "non ok connection\n";
 			break; 
 		}
 	
 		if (preg_match_all('/EndMessage/si', $buffer, $match)) { 
-			//echo "ok connection\n";
+			$ok_hello = 1;
 			break; 
 		}
-	
 	}
 
-	$out = "ClientGet\r\n";
-	$out .= "URI=$addresse\r\n";
-	$out .= "Identifier=Request Number One\r\n";
-	$out .= "ReturnType=NONE\r\n";
-	$out .= "Verbosity=1\r\n";
-	$out .= "EndMessage\r\n";
+	if ($ok_hello == 1) {
+		$out = "ClientGet\r\n";
+		$out .= "URI=$addresse\r\n";
+		$out .= "Identifier=Request Number One\r\n";
+		$out .= "ReturnType=NONE\r\n";
+		$out .= "Verbosity=1\r\n";
+		$out .= "EndMessage\r\n";
+		
+		fwrite($fp, $out);
+		$count = 1;
+		while ( !feof($fp) )
+		{  
+			$buffer = fgets($fp);
 	
-	fwrite($fp, $out);
-	$count = 1;
-	while ( !feof($fp) )
-	{  
-		$buffer = fgets($fp);
-
-		if (preg_match_all('/ExtraDescription/si', $buffer, $match)) { 
-			$ExtraDescription = explode("=",$buffer);
-			$ExtraDescription = $ExtraDescription[1];
-			echo "echec recup cle : $ExtraDescription";
-			break;
-		}
-		if (preg_match_all('/Metadata.ContentType=/si', $buffer, $match)) { 
-			$content_type = explode("=",$buffer);
-			$content_type = $content_type[1];
-			
-			if (preg_match_all('/text\/html/si', $content_type, $match)) { 	}
-			else {
-				echo "non ok html";
+			if (preg_match_all('/ExtraDescription/si', $buffer, $match)) { 
+				$ExtraDescription = explode("=",$buffer);
+				$ExtraDescription = $ExtraDescription[1];
+				$error = "Get Failed : $ExtraDescription";
 				break;
 			}
+			if (preg_match_all('/Metadata.ContentType=/si', $buffer, $match)) { 
+				$content_type = explode("=",$buffer);
+				$content_type = $content_type[1];
+				
+				if (preg_match_all('/text\/html/si', $content_type, $match)) { 	}
+				else {
+					$error = "Error Content Type";
+					break;
+				}
+			}
+	
+			if (preg_match_all('/GetFailed/si', $buffer, $match)) { 
+				$count = 2;
+				$error = "Get Failed";
+			}
+					
+			if (preg_match_all('/EndMessage/si', $buffer, $match)) { 
+				$count = $count + 1;
+				if ($count > 2) { break; }
+			}
 		}
-		
-		if (preg_match_all('/EndMessage/si', $buffer, $match)) { 
-			$count = $count + 1;
-			if ($count > 2) { break; }
+	
+		if ($error == NULL) {
+			$out = "ClientGet\r\n";
+			$out .= "URI=$addresse\r\n";
+			$out .= "Identifier=Request Number One\r\n";
+			$out .= "ReturnType=DISK\r\n";
+			$out .= "Filename=D:\\Darknet\\bot\\test1.html\r\n";
+			$out .= "Verbosity=0\r\n";
+			$out .= "EndMessage\r\n";
+			
+			fwrite($fp, $out);
+			
+			while ( !feof($fp) )
+			{  
+				$buffer = fgets($fp);
+
+				if (preg_match_all('/CodeDescription/si', $buffer, $match)) { 
+					$CodeDescription = explode("=",$buffer);
+					$CodeDescription = $CodeDescription[1];
+					$error = "Get Failed : $CodeDescription";
+					break;
+				}
+
+				if (preg_match_all('/EndMessage/si', $buffer, $match)) { 
+					break; 
+				}
+
+				$error = "all ok";		
+			}
 		}
 	}
-
-	$out = "ClientGet\r\n";
-	$out .= "URI=$addresse\r\n";
-	$out .= "Identifier=Request Number One\r\n";
-	$out .= "ReturnType=DISK\r\n";
-	$out .= "Filename=D:\\Darknet\\bot\\test1.html\r\n";
-	$out .= "Verbosity=0\r\n";
-	$out .= "EndMessage\r\n";
 	
-	fwrite($fp, $out);
-	
-	while ( !feof($fp) )
-	{  
-		$buffer = fgets($fp);
-		echo $buffer;
-
-		if (preg_match_all('/EndMessage/si', $buffer, $match)) { 
-			break; 
-		}
-
-	}
-
-echo "ok out";
+echo $error;
 
 fclose($fp);
 }
